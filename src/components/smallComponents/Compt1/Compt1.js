829d92e8c7cff9141d4bb1a5b8831d4ffdaa6c29
Utkp5 <utkp200@gmail.com>
2022-11-21 19:03:22 +0530

    bookstore frontend

---
+++ src/components/smallComponents/Compt1/Compt1.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Compt1() {
+  return (
+    <div className='main_div'>
+      <div className='sub_div'>
+        <img src="https://bookpresstheme.com/html/assets/images/index2/icon-1.png" alt="" />
+        <h2>Experience</h2>
+        <p>Lorem ipsum dolor amet, consectetur adipiscing elit, eius Lorem dolorsit consectetur.</p>
+      </div>
+      <div className='sub_div'>
+        <img src="https://bookpresstheme.com/html/assets/images/index2/icon-2.png" alt="" />
+        <h2>Marketing Goals</h2>
+        <p>Lorem ipsum dolor amet, consectetur adipiscing elit, eius Lorem dolorsit consectetur.</p>
+      </div>
+      <div className='sub_div'>
+        <img src="https://bookpresstheme.com/html/assets/images/index2/icon-3.png" alt="" />
+        <h2>Targetting Vission</h2>
+        <p>Lorem ipsum dolor amet, consectetur adipiscing elit, eius Lorem dolorsit consectetur.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Compt1
