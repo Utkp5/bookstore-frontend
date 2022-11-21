7302d7c41e91fab191f98c86a420e797fdf6f337
Utkp5 <utkp200@gmail.com>
2022-11-21 18:53:05 +0530

    bookstore frontend

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import "./Home.css"
+import { FiPlay } from "react-icons/fi";
+
 function Home() {
   return (
-    <div className='home_div'>
+    <div>
+        <div className='home_div'>
         <div className='home_sub_div'>
-            <p className='p1'>50% discount on early purchase</p>
-            <p className='p2'>Settle Your Daily <br /> life Issue in 1 <br /><span className='span1'>Minute.</span></p>
-            <p className='p3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, eius Lorem <br />
-             ipsum dolor sit amet, consectetur adipiscing elit, eius mod tempor<br />
-            incididunt labore mod tempor
-             incididunt ut labore adipiscing.</p>
-
-             <button className='btn1'>More About Us</button>
-        </div>  
-        <div>
-            <img src="https://bookpresstheme.com/html/assets/images/index2/book-image-main.png" alt="" className='home_img'/>
+        <p className='p1'>50% discount on early purchase</p>
+        <p className='p2'>Settle Your Daily <br /> life Issue in 1 <br /><span className='span1'>Minute.</span></p>
+        <p className='p3'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, eius Lorem <br />
+        ipsum dolor sit amet, consectetur adipiscing elit, eius mod tempor<br />
+        incididunt labore mod tempor
+                incididunt ut labore adipiscing.</p>
+    
+                 <button className='btn1'>More About Us</button>
+                 <a href="https://www.youtube.com/embed/pZVZdgY25uY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"><button className='btn2'><FiPlay size={18} color = "darkturquoise"/></button><span className='span2'>Watch 3:00 min</span></a>
+            </div>  
+            <div>
+                <img src="https://bookpresstheme.com/html/assets/images/index2/book-image-main.png" alt="" className='home_img'/>
+            </div>
         </div>
-
     </div>
+    
   )
 }
 
