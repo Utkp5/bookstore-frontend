f0761e460759d0d5829fd03b84eed071392a4235
Utkp5 <utkp200@gmail.com>
2022-11-21 19:41:28 +0530

    bookstore frontend

---
+++ src/components/smallComponents/Compt2/Compt2.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Compt2() {
+  return (
+    <div className='main_div'>
+      <div>
+        <img src="https://bookpresstheme.com/html/assets/images/index2/author.png" alt="" className='compt2_img'/>
+      </div>
+      <div>
+        <p>-AUTHOR & PUBLISHER</p>
+        <p>Christina Tores Mike</p>
+        <p>Lorem ipsum dolor sit amet, con ge sectetur adipiiscing elit, eius Lorem ipsum dolor sit amet, coniscinsectetur adipiscing elit, eius mod tempor incididunt labore mod sit amet adipiscingconsectetur.</p>
+        <p>About Writting More Accomplished</p>
+        <p>Exceptionally Energetic Accomplished 10+ Granted</p>
+        <p>More Accomplished 10+ Granted</p>
+        <p>Exceptionally Energetic About Writting</p>
+        <p>Most Popular Writter In The Year</p>
+        <img src="https://bookpresstheme.com/html/assets/images/index2/signature-1.png" alt="" className='compt_img1'/>
+        <a href="/"><button>Learn More</button></a>
+      </div>
+    </div>
+  )
+}
+
+export default Compt2
