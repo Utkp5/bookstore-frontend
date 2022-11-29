7b71f5df666f3cc800c4d2374d58a7cbc5f4f5ad
Utkp5 <utkp200@gmail.com>
2022-11-29 21:02:45 +0530

    bookstore frontend

--- src/components/smallComponents/Compt3/Compt3.js
+++ src/components/smallComponents/Compt3/Compt3.js
@@ -1,13 +1,22 @@
 import React from "react";
-import "./Compt3.css"
+import "./Compt3.css";
 import { HiMinus } from "react-icons/hi";
-
 
 function Compt3() {
   return (
-    <div className="compt3_div">
-    <h3 id="compt3_h3"><HiMinus color="whitesmoke"/> BENEFITS <HiMinus color="whitesmoke"/></h3>
-    <h2 id="compt3_h4">We Help You To Grow</h2>
+    <div id="compt3_div">
+      <h3 id="compt3_h3"><HiMinus color="whitesmoke" /> BENEFITS <HiMinus color="whitesmoke" /></h3>
+      <h2 id="compt3_h4">We Help You To Grow</h2>
+      <div className="styling-example">
+        <Carousel itemsToShow={2}>
+          <Item>1</Item>
+          <Item>2</Item>
+          <Item>3</Item>
+          <Item>4</Item>
+          <Item>5</Item>
+          <Item>6</Item>
+        </Carousel>
+      </div>
     </div>
   );
 }
