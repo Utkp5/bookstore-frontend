372095f3dc820543892cbb6ec8e852befcf948d6
Utkp5 <utkp200@gmail.com>
2022-11-30 20:20:39 +0530

    bookstore frontend

--- src/components/smallComponents/Compt3/Compt3.js
+++ src/components/smallComponents/Compt3/Compt3.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Compt3.css";
+import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
 import { HiMinus } from "react-icons/hi";
 
 function Compt3() {
@@ -7,17 +9,85 @@ function Compt3() {
     <div id="compt3_div">
       <h3 id="compt3_h3"><HiMinus color="whitesmoke" /> BENEFITS <HiMinus color="whitesmoke" /></h3>
       <h2 id="compt3_h4">We Help You To Grow</h2>
-      <div className="styling-example">
-        <Carousel itemsToShow={2}>
-          <Item>1</Item>
-          <Item>2</Item>
-          <Item>3</Item>
-          <Item>4</Item>
-          <Item>5</Item>
-          <Item>6</Item>
-        </Carousel>
-      </div>
+
+
+      <Carousel
+  additionalTransfrom={0}
+  arrows={false}
+  autoPlay
+  autoPlaySpeed={3000}
+  centerMode={false}
+  className=""
+  containerClass="container-with-dots"
+  dotListClass=""
+  draggable
+  focusOnSelect={false}
+  infinite
+  itemClass=""
+  keyBoardControl
+  minimumTouchDrag={80}
+  pauseOnHover
+  renderArrowsWhenDisabled={false}
+  renderButtonGroupOutside={false}
+  renderDotsOutside={false}
+  responsive={{
+    desktop: {
+      breakpoint: {
+        max: 3000,
+        min: 1024
+      },
+      items: 3,
+      partialVisibilityGutter: 40
+    },
+    mobile: {
+      breakpoint: {
+        max: 464,
+        min: 0
+      },
+      items: 1,
+      partialVisibilityGutter: 30
+    },
+    tablet: {
+      breakpoint: {
+        max: 1024,
+        min: 464
+      },
+      items: 2,
+      partialVisibilityGutter: 30
+    }
+  }}
+  rewind={false}
+  rewindWithAnimation={false}
+  rtl={false}
+  shouldResetAutoplay
+  showDots={false}
+  sliderClass=""
+  slidesToSlide={2}
+  swipeable
+>
+
+  <div>
+    description="Fixing CSS load order/style.chunk.css incorrect in Nextjs"
+    headline="w3js.com - web front-end studio"
+    image="https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
     </div>
+  <div>
+    description="React Carousel with Server Side Rendering Support – Part 2"
+    headline="w3js.com - web front-end studio"
+    image="https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    </div>
+  <div>
+    description="Appending currency sign to a purchase form in your e-commerce site using plain JavaScript."
+    headline="w3js.com - web front-end studio"
+    image="https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    </div>
+  <div>
+    description="Fixing CSS load order/style.chunk.css incorrect in Nextjs"
+    headline="w3js.com - web front-end studio"
+    image="https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    </div>
+</Carousel>
+      </div>
   );
 }
 
