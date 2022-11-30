42797ec23b54109a24c1ad9c4eabd8b9b103458e
Utkp5 <utkp200@gmail.com>
2022-11-30 21:05:55 +0530

    bookstore frontend

--- src/components/smallComponents/Compt3/Compt3.js
+++ src/components/smallComponents/Compt3/Compt3.js
@@ -1,93 +1,41 @@
 import React from "react";
 import "./Compt3.css";
-import Carousel from "react-multi-carousel";
-// import "react-multi-carousel/lib/styles.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
 import { HiMinus } from "react-icons/hi";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 function Compt3() {
   return (
     <div id="compt3_div">
-      <h3 id="compt3_h3"><HiMinus color="whitesmoke" /> BENEFITS <HiMinus color="whitesmoke" /></h3>
-      <h2 id="compt3_h4">We Help You To Grow</h2>
-
-
-      <Carousel
-  additionalTransfrom={0}
-  arrows={false}
-  autoPlay
-  autoPlaySpeed={3000}
-  centerMode={false}
-  className=""
-  containerClass="container-with-dots"
-  dotListClass=""
-  draggable
-  focusOnSelect={false}
-  infinite
-  itemClass=""
-  keyBoardControl
-  minimumTouchDrag={80}
-  pauseOnHover
-  renderArrowsWhenDisabled={false}
-  renderButtonGroupOutside={false}
-  renderDotsOutside={false}
-  responsive={{
-    desktop: {
-      breakpoint: {
-        max: 3000,
-        min: 1024
-      },
-      items: 3,
-      partialVisibilityGutter: 40
-    },
-    mobile: {
-      breakpoint: {
-        max: 464,
-        min: 0
-      },
-      items: 1,
-      partialVisibilityGutter: 30
-    },
-    tablet: {
-      breakpoint: {
-        max: 1024,
-        min: 464
-      },
-      items: 2,
-      partialVisibilityGutter: 30
-    }
-  }}
-  rewind={false}
-  rewindWithAnimation={false}
-  rtl={false}
-  shouldResetAutoplay
-  showDots={false}
-  sliderClass=""
-  slidesToSlide={2}
-  swipeable
->
-
-  <div>
-    description="Fixing CSS load order/style.chunk.css incorrect in Nextjs"
-    headline="w3js.com - web front-end studio"
-    image="https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+    <h3 id="compt3_h3"><HiMinus color="whitesmoke" /> BENEFITS <HiMinus color="whitesmoke" /></h3>
+    <h2 id="compt3_h4">We Help You To Grow</h2>
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={30}
+      slidesPerGroup={3}
+      loop={true}
+      loopFillGroupWithBlank={true}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Pagination, Navigation]}
+      className="mySwiper"
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+      <SwiperSlide>Slide 8</SwiperSlide>
+      <SwiperSlide>Slide 9</SwiperSlide>
+    </Swiper>
     </div>
-  <div>
-    description="React Carousel with Server Side Rendering Support – Part 2"
-    headline="w3js.com - web front-end studio"
-    image="https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-    </div>
-  <div>
-    description="Appending currency sign to a purchase form in your e-commerce site using plain JavaScript."
-    headline="w3js.com - web front-end studio"
-    image="https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-    </div>
-  <div>
-    description="Fixing CSS load order/style.chunk.css incorrect in Nextjs"
-    headline="w3js.com - web front-end studio"
-    image="https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-    </div>
-</Carousel>
-      </div>
   );
 }
 
